9ef7a06d6bd359afee1ac06b19d089035f487afa
Irina Vorobyeva <isvorobyeva@mail.ru>
2022-09-16 17:17:15 +0300

    first commit

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class App {
+    run() {
+        console.log('Done');
+    }
+}
+exports.App = App;
+const app = new App();
+app.run();
